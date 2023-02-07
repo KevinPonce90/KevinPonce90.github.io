83dddb658234f27d993991bd7bfec3ea331ece48
KevinPonce <kevinponce90@gmail.com>
2023-02-07 09:33:48 -0600

    Eliminacion de BackEnd

--- backend/bridge.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'connection.php';
-session_start();
-$conn = new mysqli($host, $database, $username, $passwordDB);
-if ($conn->connect_errno) {
-    echo "ERROR AL CONECTARSE";
-    exit();
-}
-?>
--- backend/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = "localhost"; //Nombr del host
-$database = "actualizatumac_web"; // Nombre de la DB
-$username = "actualizatumac"; // Usurio
-$passwordDB = "ActLz57365+"; // Contra
-date_default_timezone_set('America/Mexico_City'); //Tiempo
-error_reporting(E_ERROR | E_WARNING | E_PARSE); //Errores
-?>
